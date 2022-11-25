ac7763ac41427e54f5154b48ab7db3c420d6d34d
Derek Zhang <chromestone@users.noreply.github.com>
2022-11-24 21:16:38 -0500

    not improvable path test script
    Simple script to print not improvable paths given edge file, source, and target.

---
+++ BB84/test_not_improvable.py
@@ -0,0 +1,18 @@
+"""
+This is meant to be run as script!
+"""
+
+import sys
+
+from network import graphFromEdgeFile
+from util import all_simple_not_improvable_paths
+
+assert len(sys.argv) > 3
+
+G = graphFromEdgeFile(sys.argv[1])
+source = sys.argv[1]
+target = sys.argv[2]
+
+for path in all_simple_not_improvable_paths(G, source, target):
+
+	print(path)
