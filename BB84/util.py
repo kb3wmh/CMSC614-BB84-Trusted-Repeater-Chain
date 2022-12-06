4407ceb1cf1f8217ab3834e6543fe25dc98ca290
Derek Zhang <chromestone@users.noreply.github.com>
2022-12-05 22:04:25 -0500

    path_list changed to set for nlt
    NI paths sometimes repeat

--- BB84/util.py
+++ BB84/util.py
@@ -115,7 +115,7 @@ def node_not_traveled(G, source, target):
 
 def node_less_traveled(G, source, target, k):
 
-    path_list = []
+    path_set = {}
 
     visits = dict.fromkeys(G.nodes, 0)
     # priority if target is a neighbor
@@ -144,14 +144,16 @@ def node_less_traveled(G, source, target, k):
 
         if a_neighbor == target:
 
-            ni_path = make_path_not_improvable(G, list(curr_path.keys()) + [target])
+            ni_path = make_path_not_improvable(G, tuple(curr_path.keys()) + (target, ))
+
             # don't update visits for target to keep it a priority
             for node in ni_path[:-1]:
 
                 visits[node] += 1
 
-            path_list.append(ni_path)
-            if len(path_list) >= k:
+            # it is possible for (ni_path in path_set) == True
+            path_set.add(ni_path)
+            if len(path_set) >= k:
 
                 break
 
@@ -160,4 +162,4 @@ def node_less_traveled(G, source, target, k):
         curr_path[a_neighbor] = None
         stack.append(list(G[a_neighbor]))
 
-    return path_list
+    return path_set
